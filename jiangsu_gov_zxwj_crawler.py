8bb508457d08416a63f97483c94e61781b6edbb1
njzhx <122135364+njzhx@users.noreply.github.com>
2026-03-04 12:23:03 +0800

    Add files via upload

--- jiangsu_gov_zxwj_crawler.py
+++ jiangsu_gov_zxwj_crawler.py
@@ -88,7 +88,7 @@ def scrape_data():
                 try:
                     detail_resp = requests.get(article_url, headers=headers, timeout=15)
                     detail_soup = BeautifulSoup(detail_resp.content, 'html.parser')
-                    content_elem = detail_soup.select_one('.content') or detail_soup.select_one('#content')
+                    content_elem = detail_soup.find('div', class_='left')
                     if content_elem:
                         content = content_elem.get_text(strip=True)
                 except Exception:
